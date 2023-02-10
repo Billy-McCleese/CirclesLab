aec61cc2d801db6851e4928569fabe5fc2db5a0c
Billy-McCleese <billymccleese@gmail.com>
2023-02-09 19:29:53 -0500

    Final Submission for Circles lab

---
+++ CirlcesLab/Circle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirclesLab;
+
+public class Circle
+
+{
+    private double radius;
+
+    public double Radius { get; set; }
+}
+
+public static class CalculateCircumferenceLab
+{
+    static double Circumference;
+    public static double CalculateCircumference(double num)
+    {
+        Circumference = num * Math.PI * 2;
+        return Circumference;
+    }
+
+    public static double CircumferenceFormatted(double num)
+    {
+        Circumference = Math.Round(Circumference, 2);
+        return Circumference;
+    }
+
+}
+
+public static class CalculateAreaeLab
+{
+    static double Area;
+    public static double CalculateArea(double num)
+    {
+        Area = (Math.PI * num * num);
+        return Area;
+    }
+
+    public static double CalculateFormattedArea(double num)
+    {
+        Area = Math.Round(Area, 2);
+        return Area;
+    }
+}
---
+++ CirlcesLab/Program.cs
@@ -0,0 +1,52 @@
+﻿using CirclesLab;
+
+Console.WriteLine("Welcome to the Circle Tester");
+//possible do while loop or for each loop to loop through if the user goes again 
+//also need something to keep track of how many attempts made
+bool Close = false;
+double CircleUserInput;
+int NumberOfCircles = 0;
+while (!Close)
+{
+
+    do
+    {
+        Console.WriteLine("Enter radius: ");
+        try
+        {
+            CircleUserInput = double.Parse(Console.ReadLine());
+            NumberOfCircles++;
+            break;
+        }
+        catch (Exception)
+        {
+
+            Console.WriteLine("That is not a valid entry.");
+            Close = false;
+        }
+    } while (true);
+ 
+            Circle CircleInput = new Circle();
+            CircleInput.Radius = CircleUserInput;
+
+            Double CircumferenceNotRounded = CalculateCircumferenceLab.CalculateCircumference(CircleUserInput);
+            Double CircumferenceRounded = CalculateCircumferenceLab.CircumferenceFormatted(CircumferenceNotRounded);
+            Console.WriteLine("Circumference: " + CircumferenceRounded);
+
+            Double AreaNotRounded = CalculateAreaeLab.CalculateArea(CircleUserInput);
+            Double AreaRounded = CalculateAreaeLab.CalculateFormattedArea(AreaNotRounded);
+            Console.WriteLine("Area: " + AreaRounded);
+
+            Console.WriteLine("Would you like to do this again (y/n)");
+            string UserContinue = (Console.ReadLine().ToLower());
+            if (UserContinue is "y")
+            {
+                Close = false;
+            }
+            else
+            {
+                Close = true;
+                Console.WriteLine("See Ya! You created " + NumberOfCircles + " Circle object(s)");
+            }
+
+}
